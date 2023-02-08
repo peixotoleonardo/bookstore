fe1c3ff41397776b78669cba4e88647fbb73ae40
Leonardo Peixoto <peixotolimaleonardo@gmail.com>
2023-02-08 14:09:28 -0300

    feat(core): create CreateAuthorUseCase use case

---
+++ libs/core/src/application/author/use-case/create-author.use-case.ts
@@ -0,0 +1,20 @@
+import { Author } from '@app/core/domain/author';
+
+import {
+  CreateAuthorCommand,
+  CreateAuthorOutput,
+  CreateAuthorPortIn,
+  SaveAuthorPortOut,
+} from '@app/core/application/author/port';
+
+export class CreateAuthorUseCase implements CreateAuthorPortIn {
+  constructor(private readonly port: SaveAuthorPortOut) {}
+
+  async execute(command: CreateAuthorCommand): Promise<CreateAuthorOutput> {
+    const author = await this.port.execute(
+      Author.newAuthor(command.name, command.description),
+    );
+
+    return CreateAuthorOutput.from(author);
+  }
+}
